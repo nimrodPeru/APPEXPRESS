94dfc71aa10c564af77e753a1701a90d68338fa3
nimrodPeru <nimrod2410@gmail.com>
2018-04-11 19:39:30 -0500

    firts commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+
+/*set  - configuraciones */
+//mostrar variable->template
+app.set('template','este es valor lo agregue')
+/*set - view engine*/
+app.set('view engine','ejs')
+/*set - directorio */
+//console.log(__dirname)
+app.set('views', __dirname + '/views')
+/*midleware*/
+app.use(
+    function(req,res,next){ 
+        console.log("request url : " + req.url) 
+        next()
+    }
+    
+);
+
+app.use((req,res,next)=>{
+    console.log("ha pasado");
+    next();
+});
+
+/* rutas -routes */
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('index.ejs'))
+app.get('/login', (req, res) => res.send('login'))
+
+
+
+app.listen(3000, function(){
+    console.log('Example app listening on port 3000!')
+    //llamar
+    console.log('el valor',app.get('template'))
+})
